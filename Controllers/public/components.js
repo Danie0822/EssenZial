350efd6530ecd2cea60225747b3ba267e71d3ec0
w0lf3 <VRMOTA>
2024-03-08 15:25:01 -0600

    Pagina principal terminada

--- Controllers/public/components.js
+++ Controllers/public/components.js
@@ -39,49 +39,50 @@ document.addEventListener("DOMContentLoaded", loadFooter);
 
 
 function loadHeader(){
-    var headerContent = `        <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="../../resources/img/icono.png" width="30" height="30" class="d-inline-block align-top"
-                alt="">
-            EssenZial
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="d-flex">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Hombres</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Mujer</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Niños</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Unisex</a>
-                    </li>
-                </ul>
-                <button class="btn btn-sm bg-light peque" type="button">
-                    <a class="navbar-brand" href="../public/index.html">
-                        <img src="../../resources/img/icons8-usuario-32.png" alt=""
-                            class="d-inline-block align-text-top" id="image">
-                    </a>
-                </button>
-                <button class="btn btn-sm bg-light peque" type="button" id="boton">
-                    <a class="navbar-brand" href="../public/index.html">
-                        <img src="../../resources/img/carrito.png" alt="" class="d-inline-block align-text-top"
-                            id="image">
-                    </a>
-                </button>
+    var headerContent = `
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../../resources/img/icono.png" width="30" height="30" class="d-inline-block align-top"
+                    alt="">
+                EssenZial
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="d-flex">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Hombres</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Mujer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Niños</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Unisex</a>
+                        </li>
+                    </ul>
+                    <button class="btn btn-sm bg-light peque" type="button">
+                        <a class="navbar-brand" href="../public/index.html">
+                            <img src="../../resources/img/icons8-usuario-32.png" alt=""
+                                class="d-inline-block align-text-top" id="image">
+                        </a>
+                    </button>
+                    <button class="btn btn-sm bg-light peque" type="button" id="boton">
+                        <a class="navbar-brand" href="../public/index.html">
+                            <img src="../../resources/img/carrito.png" alt="" class="d-inline-block align-text-top"
+                                id="image">
+                        </a>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-</nav>`
+    </nav>`
 
 var header = document.querySelector("header");
 header.innerHTML = headerContent + header.innerHTML;
